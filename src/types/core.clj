67d5b99b08756ee5b18f01e22ee062908836f90a
elpinal <elpinal@users.noreply.github.com>
2018-02-16 21:51:28 +0900

    Use map-term to simplify shift-above

--- src/types/core.clj
+++ src/types/core.clj
@@ -18,10 +18,7 @@
 (defn shift-above
   "Shift above indices of variables protecting one under d"
   [t c d]
-  (case (:type t)
-    :var (let [n (:value t)] (if (< n c) t (assoc t :value (+ n d))))
-    :abs (shift-above (:value t) (+ c 1) d)
-    :app (-> t (update :fn shift-above c d) (update :arg shift-above c d))))
+  (let [f #(if (< % c) {:type :var, :value %} {:type :var, :value (+ % d)})] (map-term t f c)))
 
 (defn shift
   "Shift above indices of variables"
