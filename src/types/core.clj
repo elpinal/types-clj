c58fccb4138050e1d68bdd6b392412892d171a91
elpinal <elpinal@users.noreply.github.com>
2018-02-16 21:46:21 +0900

    Add map-term

--- src/types/core.clj
+++ src/types/core.clj
@@ -5,6 +5,16 @@
   [x]
   (println x "Hello, World!"))
 
+(defn map-term
+  "Map a term"
+  [t f c]
+  (let [walk (fn [t c g]
+               (case (:type t)
+                 :var (f (:value t))
+                 :abs (update t :value g (+ c 1) g)
+                 :app (-> t (update :fn g c g) (update :arg g c g))))]
+    (walk t c walk)))
+
 (defn shift-above
   "Shift above indices of variables protecting one under d"
   [t c d]
