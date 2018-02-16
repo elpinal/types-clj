60e35c47d0f4fb00d2e9907dfba2e0a7a67af109
elpinal <elpinal@users.noreply.github.com>
2018-02-16 21:55:08 +0900

    Test "shift"

--- test/types/core_test.clj
+++ test/types/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [types.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest shift-test
+  (testing "shifting"
+    (is (= (shift {:type :var, :value 0} 1) {:type :var, :value 1}))))
