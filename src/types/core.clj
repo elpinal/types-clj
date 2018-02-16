047b1fa3504c2ca3962457f93d53b696a7f9145b
elpinal <elpinal@users.noreply.github.com>
2018-02-16 21:17:36 +0900

    Format

--- src/types/core.clj
+++ src/types/core.clj
@@ -11,5 +11,4 @@
   (case (:type t)
     :var (let [n (:value t)] (if (< n c) t (assoc t :value (+ n d))))
     :abs (shift-above (:value t) (+ c 1) d)
-    :app (-> t (update :fn shift-above c d) (update :arg shift-above c d)))
-  )
+    :app (-> t (update :fn shift-above c d) (update :arg shift-above c d))))
