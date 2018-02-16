d8b30a7f56e90c6772159c6bcabef2cb0db0f3cc
elpinal <elpinal@users.noreply.github.com>
2018-02-16 21:16:28 +0900

    Add shift-above

--- src/types/core.clj
+++ src/types/core.clj
@@ -4,3 +4,12 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn shift-above
+  "Shift above indices of variables"
+  [t c d]
+  (case (:type t)
+    :var (let [n (:value t)] (if (< n c) t (assoc t :value (+ n d))))
+    :abs (shift-above (:value t) (+ c 1) d)
+    :app (-> t (update :fn shift-above c d) (update :arg shift-above c d)))
+  )
