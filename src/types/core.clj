c8cf255f7ddcca31159bfdfe22abcf925d2005af
elpinal <elpinal@users.noreply.github.com>
2018-02-17 13:16:43 +0900

    Change the structure of lammbda terms
    Also, `shift-above` is fixed.

--- src/types/core.clj
+++ src/types/core.clj
@@ -1,21 +1,27 @@
 (ns types.core)
 
-(defn variable [n] {:type :var, :value n})
+(defn variable [n] {:var n})
+(defn abs [t] {:abs t})
+(defn app [f x] {:app {:fn f, :arg x}})
 
 (defn map-term
   "Map a term"
   [t f c]
-  (let [walk (fn [t c g]
-               (case (:type t)
-                 :var (f (:value t))
-                 :abs (update t :value g (inc c) g)
-                 :app (-> t (update :fn g c g) (update :arg g c g))))]
+  (let [app' #((partial array-map :app) %)
+        walk (fn [t c g]
+               (condp #(%2 %1) t
+                     :var :>> #(f c %)
+                     :abs (update t :abs g (inc c) g)
+                     :app :>> #(-> %
+                                   (update :fn g c g)
+                                   (update :arg g c g)
+                                   app')))]
     (walk t c walk)))
 
 (defn shift-above
   "Shift above indices of variables protecting one under d"
   [t c d]
-  (let [f #(if (< % c) (variable %) (variable (+ % d)))] (map-term t f c)))
+  (let [f #(if (< %2 %1) (variable %2) (variable (+ %2 d)))] (map-term t f c)))
 
 (defn shift
   "Shift above indices of variables"
--- test/types/core_test.clj
+++ test/types/core_test.clj
@@ -4,4 +4,12 @@
 
 (deftest shift-test
   (testing "shifting"
-    (is (= (shift (variable 0) 1) (variable 1)))))
+    (are [x n y] (= (shift x n) y)
+      (variable 0) 1 (variable 1)
+      (abs (variable 0)) 3 (abs (variable 0))
+      (abs (variable 1)) 3 (abs (variable 4))
+      (app (variable 1) (variable 2)) 8 (app (variable 9) (variable 10))
+      (app (abs (variable 0)) (variable 2)) 8 (app (abs (variable 0)) (variable 10))
+      (variable 0) 0 (variable 0)
+      (variable 0) -1 (variable -1)
+      (variable 1) -1 (variable 0))))
